a721e9a270f54d473cc50434ac2bac1b118e24ad
Skqlixur <jmjmbo5@gmail.com>
2022-10-09 00:07:39 +0800

    Add: Index.html, style.css, script.js, and README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# word-repeat-notifier
-Tiny website to count how many words, as well as list how many occurences of each word there are.
+# Word Repeat Notifier
+
+Tiny website to count how many words, as well as list how many occurences of each word there are in some text.
+
+this was made in a coding competition with [ricsirogi](https://github.com/ricsirogi), he also made his own version but mine is better I WON BTW
+
+# TODO:
+- make it more efficient (this is never happening)
+- make it look better (this is never happening either)
---
+++ js/script.js
@@ -0,0 +1,64 @@
+let input
+let inputArr
+let wordCount
+let prevValue = 0
+let usedWords = {}
+
+function doStuff() {
+  input = document.querySelector('.essay-input').value.toLowerCase()
+  inputArr = input.split(/ +/)
+  wordCount = inputArr.length
+
+  for (let i = 0; i < wordCount; i++) {
+    prevValue = usedWords[`${inputArr[i]}`]
+    usedWords[`${inputArr[i]}`] = 1
+    if (prevValue) usedWords[`${inputArr[i]}`] = prevValue + 1
+  }
+
+  console.log(usedWords)
+  for (const property in usedWords) {
+    if (usedWords[property] === 1) delete usedWords[property]
+  }
+
+  const sortObject = obj => {
+    const sorter = (b, a) => {
+      return obj[a] - obj[b]
+    }
+    const keys = Object.keys(obj)
+    keys.sort(sorter)
+    const res = {}
+    keys.forEach(key => {
+      res[key] = obj[key]
+    })
+    return res
+  }
+
+  usedWords = sortObject(usedWords)
+
+  let msg = ''
+
+  for (const property in usedWords) {
+    msg += `${property}: ${usedWords[property]}<br>`
+  }
+
+  let countdown = 10
+  const countdownIntv = setInterval(() => {
+    document.querySelector(
+      '.total-word-count'
+    ).innerHTML = `Please wait ${countdown} seconds while we display ads to you`
+    countdown--
+    if (countdown === 0) clearInterval(countdownIntv)
+  }, 1000)
+  setTimeout(() => {
+    document.querySelector('.total-sentence-count').innerHTML =
+      'Total Sentence Count: ' + (input.match(/\./g) || []).length
+
+    document.querySelector(
+      '.total-word-count'
+    ).innerHTML = `Total Word Count: ${wordCount}`
+    document.querySelector('.words-list').innerHTML = msg
+    input = wordCount = inputArr = null
+    prevValue = 0
+    usedWords = {}
+  }, 11000)
+}
